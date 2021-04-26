20196476512414c585d23cfc2ef2ce2ca85b86f6
Dilshod-1 <81002878+Dilshod-1@users.noreply.github.com>
2021-04-27 02:52:36 +0300

    Add files via upload

---
+++ Bot.java
@@ -0,0 +1,36 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class Bot extends TelegramLongPollingBot {
+
+    private static final String TOKEN = "1773857023:AAHC07oiglnOGXyEz7xanQBa3bSNl2y5bE0";
+    private static final String USERNAME = "myKurbonov_bot";
+
+    public String getBotUsername() {
+        return USERNAME;
+    }
+
+    public String getBotToken() {
+        return TOKEN;
+    }
+
+
+
+    public void onUpdateReceived(Update update) {
+        if(update.getMessage()!=null && update.getMessage().hasText()) {
+            String chat_id = update.getMessage().getChatId().toString();
+
+
+            try {
+                execute(new SendMessage(chat_id, "Здравствуйте Дилшод"+ update.getMessage().getText()));
+                execute(new SendMessage(chat_id, "как твои дела?"+ update.getMessage().getText()));
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+}
